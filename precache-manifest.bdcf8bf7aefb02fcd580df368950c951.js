c4e25faac61427e6cd3a3361c9bf59b8d4ff8247
inícius Ponciano <viniciusponciano@gmail.com>
2019-09-06 15:52:03 -0300

    Updates

---
+++ precache-manifest.bdcf8bf7aefb02fcd580df368950c951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231c7d0e6268e787bd9b990300c72434",
+    "url": "/breaking-bad/index.html"
+  },
+  {
+    "revision": "b2c5c213997d3bbc5751",
+    "url": "/breaking-bad/static/css/main.8230c844.chunk.css"
+  },
+  {
+    "revision": "5c193a30da957055494b",
+    "url": "/breaking-bad/static/js/2.1a2a17fb.chunk.js"
+  },
+  {
+    "revision": "b2c5c213997d3bbc5751",
+    "url": "/breaking-bad/static/js/main.58eab2ab.chunk.js"
+  },
+  {
+    "revision": "3ca274be1c68648e06fe",
+    "url": "/breaking-bad/static/js/runtime~main.e4ddf979.js"
+  }
+]);
